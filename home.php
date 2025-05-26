2d171056fdaa0174a28d67eaaec6fb6cf94512bf
Saku0512 <moonandstar118@gmail.com>
2025-05-27 02:57:13 +0900

    ref: headerをテンプレート化

--- home.php
+++ home.php
@@ -18,26 +18,7 @@ header("Content-Security-Policy:
     <link rel="stylesheet" href="./css/home.css" nonce="<?= htmlspecialchars($nonce, ENT_QUOTES, 'UTF-8') ?>">
 </head>
 <body>
-    <header>
-        <div class="mini_logo">
-            <img src="https://picsum.photos/80" />
-        </div>
-        <div class="index">
-            <p class="title">高専祭2025</p>
-            <div class="subtitles">
-                <p class="subtitle">ご挨拶</p>
-                <p class="subtitle">企画一覧</p>
-                <p class="subtitle">アクセス</p>
-                <p class="subtitle">ニュース</p>
-                <p class="subtitle">Q&A</p>
-                <p class="subtitle">お問い合わせ</p>
-                <p class="subtitle">プライバシーポリシー</p>
-            </div>
-        </div>
-        <div class="menu">
-            <img src="https://picsum.photos/80" alt="Menu Icon" />
-        </div>
-    </header>
+    <?php include_once 'includes/header.php'; ?>
     <main>
         <div class="main_logo">
             <img src="https://picsum.photos/400/600" alt="Main Image" />
