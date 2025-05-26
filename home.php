acc5266cab18991c2feabc4e71c4c28a78f160db
Saku0512 <moonandstar118@gmail.com>
2025-05-27 02:44:11 +0900

    feat:home画面を作り始めた

--- home.php
+++ home.php
@@ -15,8 +15,33 @@ header("Content-Security-Policy:
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="./css/home.css" nonce="<?= htmlspecialchars($nonce, ENT_QUOTES, 'UTF-8') ?>">
 </head>
 <body>
-    <h1>Welcome</h1>
+    <header>
+        <div class="mini_logo">
+            <img src="https://picsum.photos/80" />
+        </div>
+        <div class="index">
+            <p class="title">高専祭2025</p>
+            <div class="subtitles">
+                <p class="subtitle">ご挨拶</p>
+                <p class="subtitle">企画一覧</p>
+                <p class="subtitle">アクセス</p>
+                <p class="subtitle">ニュース</p>
+                <p class="subtitle">Q&A</p>
+                <p class="subtitle">お問い合わせ</p>
+                <p class="subtitle">プライバシーポリシー</p>
+            </div>
+        </div>
+        <div class="menu">
+            <img src="https://picsum.photos/80" alt="Menu Icon" />
+        </div>
+    </header>
+    <main>
+        <div class="main_logo">
+            <img src="https://picsum.photos/400/600" alt="Main Image" />
+        </div>
+    </main>
 </body>
 </html>
