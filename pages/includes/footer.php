7a8894e2d9af01e07d16ca3e8f4f328057bc9f2d
Saku0512 <moonandstar118@gmail.com>
2025-06-09 18:24:11 +0900

    footerを画面サイズによって変える

--- pages/includes/footer.php
+++ pages/includes/footer.php
@@ -1,14 +1,19 @@
 <footer>
     <div class="footer-content">
-        <p class="title">ホーム</p>
-        <div class="subtitles">
-            <p class="subtitle">ご挨拶</p>
-            <p class="subtitle">企画一覧</p>
-            <p class="subtitle">アクセス</p>
-            <p class="subtitle">ニュース</p>
-            <p class="subtitle">Q&A</p>
-            <p class="subtitle">お問い合わせ</p>
-            <p class="subtitle">プライバシーポリシー</p>
+        <div class="footer_menu">
+            <a class="title" href="/home.php">ホーム</a>
+            <div class="subtitles">
+                <p class="subtitle">ご挨拶</p>
+                <p class="subtitle">企画一覧</p>
+                <p class="subtitle">アクセス</p>
+                <p class="subtitle">ニュース</p>
+                <p class="subtitle">Q&A</p>
+                <a class="subtitle" href="/pages/contact.php">お問い合わせ</a>
+                <p class="subtitle">プライバシーポリシー</p>
+            </div>
+        </div>
+        <div>
+            <P>© 2025 高専祭実行委員会 - 仙台高等専門学校広瀬キャンパス</P>
         </div>
     </div>
 </footer>
