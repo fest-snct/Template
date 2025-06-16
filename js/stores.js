dccd3195380a1460cefbc29136754e78dbed0e8f
nanamo-7nl <sweetsugar4503@gmail.com>
2025-06-16 22:39:21 +0900

    element.addEventListenerを使うように修正

--- js/stores.js
+++ js/stores.js
@@ -1,17 +1,17 @@
 var glb_iIndex = 0;
-function showModal(elm, img_id) {
+function showModal(e) {
     const modal_img = document.getElementById("modal_img");
     const modal_title = document.getElementById("modal_title");
     const modal = document.getElementById("modal");
-    const self_img = elm.getElementsByTagName("img")[0];
+    const self_img = e.currentTarget.getElementsByTagName("img")[0];
     const modal_txt = document.getElementById("modal_txt");
     const figs = document.getElementsByTagName("main")[0].getElementsByClassName("s_items");
     const matchRx = /[ 　]場所[:：][ 　]?\d{1,2}-\d{3}$/;
     const matchAlt = self_img.getAttribute("alt").match(matchRx);
-    glb_iIndex = img_id;
+    glb_iIndex = this.img_id;
     modal_place.innerText = matchAlt != null ? matchAlt[0].replace(/^[ 　]/, "", 1) : "";
     modal_title.innerText = self_img.getAttribute("alt").replace(matchRx, "");
-    modal_txt.innerText = figs[img_id].getAttribute("data-description");
+    modal_txt.innerText = figs[this.img_id].getAttribute("data-description");
     modal_img.src = self_img.src;
     modal_img.alt = self_img.alt;
     modal.removeAttribute("class");
@@ -26,7 +26,8 @@ function closeModal(e) {
         modal.setAttribute("class", "nodisp");
     }
 }
-function moveModal(dir){
+function moveModal(e){
+    const dir = this.dir;
     const figs = document.getElementsByTagName("main")[0].getElementsByClassName("s_items");
     glb_iIndex += dir;
     if (glb_iIndex >= figs.length) {
@@ -34,10 +35,28 @@ function moveModal(dir){
     } else if (glb_iIndex < 0){
         glb_iIndex = figs.length - 1;
     }
-    showModal(
-        figs[glb_iIndex].getElementsByTagName("a")[0],
-        glb_iIndex
-    )
+    
+    const modal_img = document.getElementById("modal_img");
+    const modal_title = document.getElementById("modal_title");
+    const modal = document.getElementById("modal");
+    const self_img = figs[glb_iIndex].getElementsByTagName("img")[0];
+    const modal_txt = document.getElementById("modal_txt");
+    const matchRx = /[ 　]場所[:：][ 　]?\d{1,2}-\d{3}$/;
+    const matchAlt = self_img.getAttribute("alt").match(matchRx);
+    modal_place.innerText = matchAlt != null ? matchAlt[0].replace(/^[ 　]/, "", 1) : "";
+    modal_title.innerText = self_img.getAttribute("alt").replace(matchRx, "");
+    modal_txt.innerText = figs[glb_iIndex].getAttribute("data-description");
+    modal_img.src = self_img.src;
+    modal_img.alt = self_img.alt;
+    modal.removeAttribute("class");
 }
-//addEventListener('click', closeModal);
-//https://pote-chil.com/posts/close-dialog-by-backdrop
+window.onload = function () {
+  const figs = document.getElementsByTagName("main")[0].getElementsByClassName("s_items");
+  for (let i = 0; i < figs.length; i++) {
+    figs[i].addEventListener("click", {img_id: i, handleEvent: showModal});
+  }
+  document.getElementById("modal_inner").addEventListener("click", closeModal);
+  document.getElementById("modal_head").getElementsByClassName("close-button")[0].addEventListener("click", closeModal);
+  document.getElementById("next").addEventListener("click", {dir: 1, handleEvent:moveModal});
+  document.getElementById("prev").addEventListener("click", {dir: -1, handleEvent:moveModal});
+}
--- pages/stores.php
+++ pages/stores.php
@@ -11,6 +11,7 @@ header("Content-Security-Policy:
 ?>
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,34 +19,29 @@ header("Content-Security-Policy:
     <link rel="stylesheet" href="/css/stores.css" nonce="<?= htmlspecialchars($nonce, ENT_QUOTES, 'UTF-8') ?>">
     <script src="/js/stores.js" nonce="<?= htmlspecialchars($nonce, ENT_QUOTES, 'UTF-8') ?>" defer></script>
 </head>
+
 <body>
     <?php include_once './includes/header.php'; ?>
     <main>
         <h1>企画一覧</h1>
         <div id="s_container">
             <figure class="s_items" data-description="詳細1">
-                <a href="javascript:void(0)" onclick="showModal(this,0)">
-                    <img src="https://img.skin/420x315?text=%E6%BA%96%E5%82%99%E4%B8%AD%E3%81%A7%E3%81%99...&fmt=gif" class="s_pic" alt="企画1" />
-                </a>
+                <img src="https://img.skin/420x315?text=%E6%BA%96%E5%82%99%E4%B8%AD%E3%81%A7%E3%81%99...&fmt=gif" class="s_pic" alt="企画1" />
             </figure>
             <figure class="s_items" data-description="詳細2">
-                <a href="javascript:void(0)" onclick="showModal(this,1)">
-                    <img src="https://img.skin/420x315?text=%E6%BA%96%E5%82%99%E4%B8%AD%E3%81%A7%E3%81%99...&fmt=gif" class="s_pic" alt="企画2" />
-                </a>
+                <img src="https://img.skin/420x315?text=%E6%BA%96%E5%82%99%E4%B8%AD%E3%81%A7%E3%81%99...&fmt=gif" class="s_pic" alt="企画2" />
             </figure>
             <figure class="s_items" data-description="詳細3">
-                <a href="javascript:void(0)" onclick="showModal(this,2)">
-                    <img src="https://img.skin/420x315?text=%E6%BA%96%E5%82%99%E4%B8%AD%E3%81%A7%E3%81%99...&fmt=gif" class="s_pic" alt="企画3" />
-                </a>
+                <img src="https://img.skin/420x315?text=%E6%BA%96%E5%82%99%E4%B8%AD%E3%81%A7%E3%81%99...&fmt=gif" class="s_pic" alt="企画3" />
             </figure>
         </div>
     </main>
     <div id="modal" class="nodisp">
         <div id="modal_bg"></div>
-        <div id="modal_inner" onclick="closeModal(event)">
+        <div id="modal_inner">
             <div id="modal_root">
                 <div id="modal_head">
-                    <button class="close-button" onclick="closeModal(event)">
+                    <button class="close-button">
                         <div class="close-button__line"></div>
                         <div class="close-button__line"></div>
                     </button>
@@ -61,10 +57,11 @@ header("Content-Security-Policy:
                     出店詳細
                 </div>
             </div>
-            <a class="prev modal_pn" onclick="moveModal(-1)">❮</a>
-            <a class="next modal_pn" onclick="moveModal(1)">❯</a>
+            <a id="prev" class="modal_pn">❮</a>
+            <a id="next" class="modal_pn">❯</a>
         </div>
     </div>
     <?php include_once './includes/footer.php'; ?>
 </body>
+
 </html>
