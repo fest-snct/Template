305a886df5d82ad506ec21d34203cc6b6d089742
Saku0512 <moonandstar118@gmail.com>
2025-08-16 22:28:09 +0900

    ハンバーガーメニューのパスを絶対パスで指定

--- pages/includes/hamburgermenu.php
+++ pages/includes/hamburgermenu.php
@@ -4,14 +4,14 @@
     <div class="wave"></div>
 
     <div class="hamburger-menu__inner">
-        <a href="../home.php" class="hamburger-menu__item title">ホーム</a>
-        <a href="./greeting.php" class="hamburger-menu__item">ご挨拶</a>
-        <a href="./event.php" class="hamburger-menu__item">イベント企画</a>
-        <a href="./stores.php" class="hamburger-menu__item">出店一覧</a>
-        <a href="./access.php" class="hamburger-menu__item">アクセス</a>
-        <a href="./news.php" class="hamburger-menu__item">ニュース</a>
-        <a href="./Q&A.php" class="hamburger-menu__item">Q&A</a>
-        <a href="./contact.php" class="hamburger-menu__item">お問い合わせ</a>
-        <a href="./privacypolicy.php" class="hamburger-menu__item">プライバシーポリシー</a>
+        <a href="/2025/home.php" class="hamburger-menu__item title">ホーム</a>
+        <a href="/2025/pages/greeting.php" class="hamburger-menu__item">ご挨拶</a>
+        <a href="/2025/pages/event.php" class="hamburger-menu__item">イベント企画</a>
+        <a href="/2025/pages/stores.php" class="hamburger-menu__item">出店一覧</a>
+        <a href="/2025/pages/access.php" class="hamburger-menu__item">アクセス</a>
+        <a href="/2025/pages/news.php" class="hamburger-menu__item">ニュース</a>
+        <a href="/2025/pages/Q&A.php" class="hamburger-menu__item">Q&A</a>
+        <a href="/2025/pages/contact.php" class="hamburger-menu__item">お問い合わせ</a>
+        <a href="/2025/pages/privacypolicy.php" class="hamburger-menu__item">プライバシーポリシー</a>
     </div>
 </nav>
