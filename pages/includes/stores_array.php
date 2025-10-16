ac7cef1ca905e83b17c7c41292a0a31d58bab18d
hachi-mori <107915908+hachi-mori@users.noreply.github.com>
2025-10-16 21:41:32 +0900

    IT2の出店説明文に変更依頼があったので対応（直前のコミットはミス）

--- pages/includes/stores_array.php
+++ pages/includes/stores_array.php
@@ -69,7 +69,7 @@ $stores = [
     [
         'id' => 'ennnichiIT2',
         'alt' => '縁日(IT2) 場所: 広場',
-        'description' => '一回100円の射的です。的に当てるとお菓子をし一掴みできます。',
+        'description' => '1回100円の射的です。狙ったお菓子に命中させてゲットしてください！',
         'image' => '/2025/images/stores/ennnichiIT2.webp'
     ],
     [
