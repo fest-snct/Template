027d1a477e3c73c852bf7189f295234e8977ad45
Saku0512 <moonandstar118@gmail.com>
2025-10-24 14:23:18 +0900

    feat: 軽音楽部のnews

--- pages/news.php
+++ pages/news.php
@@ -32,6 +32,17 @@ header("Content-Security-Policy:
             <p class="title">ニュース一覧</p>
             <div class="news_list">
                 <div class="news_item">
+                    <a href="./news/11.php">
+                        <div class="news_item_top">
+                            <img src="../images/news/keionn_news.webp" alt="News Image">
+                        </div>
+                    </a>
+                    <div class="news_item_bottom">
+                        <p class="news_date">2025.10.24</p>
+                        <p class="news_title"><a href="./news/11.php">軽音楽部ライブのお知らせ</a></p>
+                    </div>
+                </div>
+                <div class="news_item">
                     <a href="./news/10.php">
                         <div class="news_item_top">
                             <img src="../images/stores/DTMbu.webp" alt="News Image">
